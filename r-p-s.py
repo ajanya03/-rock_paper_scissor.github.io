e3a384ed2451f16b983855aae6dac9d0aa09133b
ajanya03 <130043690+ajanya03@users.noreply.github.com>
2024-08-02 22:06:31 +0530

    initial code

---
+++ r-p-s.py
@@ -0,0 +1,14 @@
+import random
+
+guesses = ['ROCK','PAPER', 'SCISSOR']
+
+def rock_paper_scissor(guesses):
+
+    while True:
+        user_input = input("Enter 1 to play 0 to quit : ")
+        if user_input == '0':
+            print("We are sorry to see you leave!!!")
+            break
+        else:
+            
+    
