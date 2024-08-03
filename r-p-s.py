0db3bb00d5579bf5db4c0598395c230c7a19b7e9
ajanya03 <130043690+ajanya03@users.noreply.github.com>
2024-08-03 10:06:06 +0530

    Update r-p-s.py

--- r-p-s.py
+++ r-p-s.py
@@ -1,8 +1,17 @@
 import random
 
-guesses = ['ROCK','PAPER', 'SCISSOR']
+guesses = ['rock','paper', 'scissor']
 
-def rock_paper_scissor(guesses):
+def who_wins(player, opponent):
+    if player == 'paper' and opponent == 'scissor' or player == 'scissor' and opponent == 'paper' or player == 'rock' and opponent == 'scissor':
+        print("You Won !!! . Computer Lose !!!")
+    elif player == opponent:
+        print("It's a TIE !!!")
+    else:
+        print("Computer Won!!! . You lose!!!")
+
+
+def rock_paper_scissor():
 
     while True:
         user_input = input("Enter 1 to play 0 to quit : ")
@@ -10,5 +19,11 @@ def rock_paper_scissor(guesses):
             print("We are sorry to see you leave!!!")
             break
         else:
-            
-    
+            # p > r , s > p , r > s
+            player = input("Enter rock/paper/scissor to begin : ").lower()
+            opponent = random.choice(guesses)
+            print(f"Computer chooses {opponent}")
+            who_wins(player, opponent)
+
+
+rock_paper_scissor()
